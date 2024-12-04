aa7555c111306a2633b00f34e070828488685bce
Ioshua <126164539+Ioshua-N@users.noreply.github.com>
2024-12-03 21:19:22 -0300

    card page completa v1

---
+++ card-page/card-page.js
@@ -0,0 +1,9 @@
+const correctButton = document.getElementById('btn-right');
+correctButton.addEventListener('click', () => {
+    window.location.href = '../roll-dice/roll-dice.html'
+});
+
+const wrongButton = document.getElementById('btn-wrong');
+wrongButton.addEventListener('click', () => {
+    window.location.href = '../roll-dice/roll-dice.html'
+});
